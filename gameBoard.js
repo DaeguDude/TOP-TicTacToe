130347116001d85d8e95cab6fa3bb577ed359ccf
SangHak <k3hppk@gmail.com>
2020-07-20 17:40:51 +0900

    Task 5 almost done, I will comeback later

--- gameBoard.js
+++ gameBoard.js
@@ -1,42 +1,58 @@
 // How do you check the winner?
 
 let board = [
-  'X', 'X', 'X',
-  'X', '', 'O',
+  '', '', '',
+  '', 'X', '',
   'X', 'X', 'X'
 ]
 
 const checkTheWinner = () => {
-    
   /**
+   * There are 8 ways to win in tic-tac-toe games.
+   * 
    * row - 0-2, 3-5, 6-8
    * column - [0,3,6], [1,4,7], [2,5,8]
-   * diagonal - [0, 4, 8], [2, 4, 6] */ 
+   * diagonal - [0, 4, 8], [2, 4, 6]
+   */ 
   
-  // ROW
+  // Rows
   let x = 0;
-  for(let i=0; i<3; i++) {
-    if(board[x] === board[x+1] && board[x] === board[x+2]) {
-      console.log('A row has all same characters!')
+  for(let i = 0; i < 3; i++) {
+    if(board[x] === board[x+1] && board[x+1] === board[x+2]) {
+      // Finish the game
     }
-    x = x+3;
+    // In the rows, by adding 3 to the variable 'x', you will
+    // change the row
+    x += 3;
   }
 
-  // COLUMN
-  for(let i=0; i<3; i++) {
-    if(board[i] === board[i+3] && board[i] === board[i+6]) {
-      console.log('A column has all same characters!')
+  // Columns
+  x = 0;
+  for(let i = 0; i < 3; i++) {
+    if(board[x] === board[x+3] && board[x+3] === board[x+6]) {
+      // Finish the game
     }
+    // By adding 1 to the index, it's chanigng to the next columns.
+    x += 1;
   }
 
-  // DIAGONAL
-  if(board[0] === board[4] && board[0] === board[8]) {
-    console.log('From Top left to Bottom Right is same');
-  }
+  // Diagonals
+  x = 0;
+  for(let i = 0; i < 2; i++) {
+    if(board[x] === board[4] && board[4] === board[8-x]) {
+      // Finish the game
+    }
 
-  if(board[2] === board[4] && board[2] === board[6]) {
-    console.log('From Top Right to Bottom Left is same');
+    // By adding 2 to the index, it's changing to the different
+    // diagonal direction
+    x += 2;
   }
 }
 
+
 checkTheWinner();
+
+
+
+  
+
